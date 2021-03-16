908ab9323267eeb7d4e079866b3b181af7fdc6be
ninghe <291784082@qq.com>
2021-03-16 11:27:53 +0800

    Create MIBC_subtype.R

---
+++ MIBC_subtype.R
@@ -0,0 +1,35 @@
+##################################################################################################
+####                         consensusMIBC Subtyping Classifer                                ####
+##################################################################################################
+MIBCSubtyping <- function(countMat, geneID = "SYMBOL", normalized = FALSE){
+  suppressMessages(require(consensusMIBC))
+  suppressMessages(require(dplyr))
+  
+  if(!normalized){countMat.norm <- log2(countNormlization(round(countMat)) + 1) %>% as.data.frame()}else{countMat.norm = countMat}
+  ####
+  if(geneID == "ENSEMBL"){
+    countMat.norm <- geneLocalAnno(exprMatrix = countMat.norm, gtf = "gencode.gene.info.v22.tsv")
+    countMat.norm <- dupRemove(countMat.norm[,-ncol(countMat.norm)])
+    geneAnno <- geneIDannotation(rownames(countMat.norm))
+    geneAnno <- as.data.frame(unique(na.omit(geneAnno)[,c("gene_id","symbol")]))
+    rownames(geneAnno) <- geneAnno$symbol
+    countMat.norm$EntrezID <- as.character(geneAnno[rownames(countMat.norm),"gene_id"])
+    countMat.norm <- dupRemove(na.omit(countMat.norm), geneID = "EntrezID") %>% as.data.frame()
+    res <- getConsensusClass(countMat.norm)
+  }
+  ####
+  if(geneID == "SYMBOL"){ 
+    geneAnno <- geneIDannotation(rownames(countMat.norm))
+    geneAnno <- as.data.frame(unique(na.omit(geneAnno)[,c("gene_id","symbol")]))
+    rownames(geneAnno) <- geneAnno$symbol
+    countMat.norm$EntrezID <- as.character(geneAnno[rownames(countMat.norm),"gene_id"])
+    countMat.norm <- dupRemove(na.omit(countMat.norm), geneID = "EntrezID") %>% as.data.frame()
+    res <- getConsensusClass(countMat.norm)
+  }
+  ####
+  if(geneID == "ENTREZ"){
+    res <- getConsensusClass(countMat.norm)
+  }
+  ####
+  return(res)
+}
