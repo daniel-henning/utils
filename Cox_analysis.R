ab20099e04c09c60517a61f3cde227dee34c3c90
ninghe <291784082@qq.com>
2021-12-24 17:34:46 +0800

    Update Cox_analysis.R

--- Cox_analysis.R
+++ Cox_analysis.R
@@ -1,7 +1,33 @@
+#### FUNCTION TO RUN SINGLE VARIABLE COXPH ANALYSIS.
+uni_coxph <- function(dat, variables, time = "time", status = "status"){
+  if(!require(survival)){install.packages("survival");require(survival)}
+  if(!require(survminer)){install.packages("survminer");require(survminer)}
+  
+  y <- Surv(time=dat[,time],event=dat[,status]==1)
+
+  Uni_cox_model<- function(x){
+    FML <- as.formula(paste0 ("y~",x))
+    cox<- coxph(FML,data=dat)
+    cox1<-summary(cox)
+    HR <- round(cox1$coefficients[,2],3)
+    PValue <- round(cox1$coefficients[,5],4)
+    CI5 <-round(cox1$conf.int[,3],3)
+    CI95 <-round(cox1$conf.int[,4],3)
+    Uni_cox_model<- data.frame( "variables" = x, 'HR' = HR, 'CI.95' = paste(CI5,"-",CI95), 'p' = PValue)
+    return(Uni_cox_model)
+  }
+  #Runing Uni-cox analysis.
+  Uni_cox <- ldply(lapply(variables, Uni_cox_model))
+  
+  return(Uni_cox)
+}
+
+
 
 #### FUNCTION TO RUN MULTI-VARIABLES COXPH ANALYSIS.
 mul_coxph <- function(dat, covariates, time = "time", status = "status"){
-
+  if(!require(survival)){install.packages("survival");require(survival)}
+  if(!require(survminer)){install.packages("survminer");require(survminer)}
   univ_formulas <- sapply(covariates, function(x)as.formula(paste(paste0('Surv(',time,',', status,')~'), x)))
 
   univ_models <- lapply( univ_formulas, function(x){coxph(x, data = dat)})
@@ -14,10 +40,9 @@ mul_coxph <- function(dat, covariates, time = "time", status = "status"){
                            HR <-signif(x$coef[2], digits=2);#exp(beta)
                            HR.confint.lower <- signif(x$conf.int[,"lower .95"], 2)
                            HR.confint.upper <- signif(x$conf.int[,"upper .95"],2)
-                           HR <- paste0(HR, " (", 
-                                        HR.confint.lower, "-", HR.confint.upper, ")")
-                           res<-c(beta, HR, wald.test, p.value)
-                           names(res)<-c("beta", "HR (95% CI for HR)", "wald.test", "p.value")
+                           CI <- paste0(HR.confint.lower, " - ", HR.confint.upper)
+                           res<-c(beta, HR, CI, wald.test, p.value)
+                           names(res)<-c("beta", "HR", "CI(95)", "wald.test", "p.value")
                            return(res)
                            #return(exp(cbind(coef(x),confint(x))))
                          })
